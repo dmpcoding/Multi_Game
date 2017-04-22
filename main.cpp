2c873dc44bd147432d753eceddbea781ddbd1729
dmpcoding <dmpgames@o2.pl>
2017-04-22 22:04:03 +0200

    add window

--- main.cpp
+++ main.cpp
@@ -3,6 +3,28 @@
 
 int main(){
 
+	sf::RenderWindow game_window(sf::VideoMode(500,500),"L_S");
+	
+	while(game_window.isOpen()){
+
+		sf::Event e;
+		while(game_window.pollEvent(e)){
+		
+			switch(e.type){
+				
+				case sf::Event::Closed:
+					game_window.close();
+			
+			}
+		
+		}
+	
+		game_window.clear();
+
+		game_window.display();
+
+	}
+
 	return 0;
 
 }
